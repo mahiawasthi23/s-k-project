e3595aadd54d43649ecbcd4f7d2fda91f17125e2
Archana Singh <archana21@navgurukul.org>
2025-04-21 21:45:39 +0530

    change service background color

--- src/Component/Service.jsx
+++ src/Component/Service.jsx
@@ -5,20 +5,7 @@ function Service() {
     return (
         <div className="service-con">
             <div className="service-background">
-            <video
-            className="service-img"
-            autoPlay
-            loop
-            muted
-            playsInline
-        >
-        <source 
-            src="https://cdn.dribbble.com/userupload/17658826/file/original-e90b2a314b04f85dfbbf198359ab5098.mp4" 
-            type="video/mp4" 
-        />
-        Your browser does not support the video tag.
-        </video>
-
+                <img src="/servicelogo.gif" alt="Animated background" className="service-img"/>
                 <div className="service-heding">
                 <h2 className="service-heading">Our Professional Services</h2>
                 <p className="service-sol">Comprehensive Home Repair and Installation Solutions</p>
