efc20a7e1109d1a8e11de0497cdf5093a54d5ff6
priya shukla <priyashukla22@navgurukul.org>
2025-04-23 17:20:13 +0530

    updated service.jxs

--- src/Component/Service.jsx
+++ src/Component/Service.jsx
@@ -1,21 +1,18 @@
 import "./Service.css"
 import ServiceList from "./ServiceList";
-
 function Service() {
     return (
         <div className="service-con">
             <div className="service-background">
-                <img src="/servicelogo.gif" alt="Animated background" className="service-img"/>
                 <div className="service-heding">
-                <h2 className="service-heading">Our Professional Services</h2>
-                <p className="service-sol">Comprehensive Home Repair and Installation Solutions</p>
+                <h1>Our Professional Services</h1>
+                <p>Comprehensive Home Repair</p>
+                <p>and Installation Solutions</p>
                 </div>
-                
             </div>
             <ServiceList/>
         </div>
     );
 }
-
 export default Service;
 
