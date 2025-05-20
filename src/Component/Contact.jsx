0b7f694e276d08cd46ebc6daa868581bffbf9c0c
Priyanka-mis <priyankamishra22@navgurukul.org>
2025-05-20 10:55:43 +0530

    did changes

--- src/Component/Contact.jsx
+++ src/Component/Contact.jsx
@@ -1,7 +1,6 @@
 import { useState } from 'react';
 import '../Component/contact.css';
 
-
 function Contact() {
   const [rotateMain, setRotateMain] = useState(false);
   const [rotateSub, setRotateSub] = useState(false);
